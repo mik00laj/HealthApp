c801f75392ddf4bf6d0d14d552e3c514573dc3d2
mik00laj <mikolaj.bania@gmail.com>
2023-12-17 20:17:36 +0100

    inkrementacja measureId po stronie serwera próba

--- Backend/main.js
+++ Backend/main.js
@@ -20,7 +20,7 @@ db.once('open', () => {
 
 const temperatureSchema = new mongoose.Schema({
   sensorId: { type: String, default: 'DS18B20' },
-  measure: { type: Number, default: 0 },
+  measureId: { type: Number, default: 0 },
   value: Number,
   date: { type: String, default: () => new Date().toISOString().split('T')[0] },
   time: { type: String, default: () => new Date().toISOString().split('T')[1].split('.')[0] }
@@ -30,7 +30,7 @@ const TemperatureModel = mongoose.model('Temperature', temperatureSchema, 'Tempe
 
 const bpmSchema = new mongoose.Schema({
   sensorId: { type: String, default: 'MAX30102_bpm' },
-  measure: { type: Number, default: 0 },
+  measureId: { type: Number, default: 0 },
   value: Number,
   date: { type: String, default: () => new Date().toISOString().split('T')[0] },
   time: { type: String, default: () => new Date().toISOString().split('T')[1].split('.')[0] }
@@ -40,12 +40,11 @@ const BPMModel = mongoose.model('Hearthbeat', bpmSchema, 'BPM');
 
 const saturationSchema = new mongoose.Schema({
   sensorId: { type: String, default: 'MAX30102_saturation' },
-  measure: { type: Number, default: 0 },
+  measureId: { type: Number, default: 0 },
   value: Number,
   date: { type: String, default: () => new Date().toISOString().split('T')[0] },
   time: { type: String, default: () => new Date().toISOString().split('T')[1].split('.')[0] }
 });
-
 
 const SaturationModel = mongoose.model('Saturation', saturationSchema, 'Saturation');
 
@@ -53,14 +52,14 @@ app.use(express.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 
-app.post('/ESP/TEMPERATURE-SENSOR', async (req, res) => {
+app.post('/esp/temperature-sensor', async (req, res) => {
   try {
     const dataTemperature = req.body;
     console.log('Received temperature data:', dataTemperature);
 
     const temperatureEntry = new TemperatureModel({ value: dataTemperature.temperature });
-    await temperatureEntry.save();
-
+    await incrementAndSave(temperatureEntry, TemperatureModel);
+    
     res.status(200).send('Zapytanie POST zostało odebrane przez serwer.');
   } catch (error) {
     console.error('Błąd podczas zapisywania danych:', error);
@@ -68,14 +67,14 @@ app.post('/ESP/TEMPERATURE-SENSOR', async (req, res) => {
   }
 });
 
-app.post('/ESP/PULS-SENSOR', async (req, res) => {
+app.post('/esp/puls-sensor', async (req, res) => {
   try {
     const dataPuls = req.body;
     console.log('Received puls data:', dataPuls);
 
     const pulsEntry = new BPMModel({ value: dataPuls.heartbeat });
-    await pulsEntry.save();
-
+    await incrementAndSave(pulsEntry, BPMModel);
+    
     res.status(200).send('Zapytanie POST zostało odebrane przez serwer.');
   } catch (error) {
     console.error('Błąd podczas zapisywania danych:', error);
@@ -83,20 +82,32 @@ app.post('/ESP/PULS-SENSOR', async (req, res) => {
   }
 });
 
-app.post('/ESP/SATURATION-SENSOR', async (req, res) => {
+app.post('/esp/saturation-sensor', async (req, res) => {
   try {
     const dataSaturation = req.body;
     console.log('Received saturation data:', dataSaturation);
 
     const saturationEntry = new SaturationModel({ value: dataSaturation.saturation });
-    await saturationEntry.save();
-
+    await incrementAndSave(saturationEntry, SaturationModel);
+    
     res.status(200).send('Zapytanie POST zostało odebrane przez serwer.');
   } catch (error) {
     console.error('Błąd podczas zapisywania danych:', error);
     res.status(500).send('Wystąpił błąd podczas zapisywania danych.');
   }
 });
+
+async function incrementAndSave(entry, model) {
+  try {
+    const lastEntry = await model.findOne({}, {}, { sort: { measureId: -1 } });
+    const nextMeasureId = lastEntry ? lastEntry.measureId + 1 : 0;
+
+    entry.measureId = nextMeasureId;
+    await entry.save();
+  } catch (error) {
+    throw error;
+  }
+}
 
 app.listen(port, () => {
   console.log(`Serwer Express nasłuchuje na porcie ${port}`);
