78926323cadb8df8ff2da07b25bb18e39180a695
mik00laj <mikolaj.bania@gmail.com>
2024-01-15 00:26:50 +0100

    poprawki od doktora Ignasiak part 4

--- Frontend/src/app/pages/tables/tables.component.ts
+++ Frontend/src/app/pages/tables/tables.component.ts
@@ -1,13 +1,27 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-tables',
   templateUrl: './tables.component.html',
   styleUrl: './tables.component.scss'
 })
-export class TablesComponent {
+export class TablesComponent implements OnInit {
   selectedStartDate: Date;
   selectedEndDate: Date;
+  formattedStartDate: string;
+  formattedEndDate: string;
+
+  constructor() {}
+
+  ngOnInit(): void {
+    const dateFromMonthAgo = new Date();
+    dateFromMonthAgo.setMonth(dateFromMonthAgo.getMonth() - 1);
+    const actualDate = new Date();
+    this.selectedStartDate = dateFromMonthAgo;
+    this.selectedEndDate = actualDate;
+    this.formattedEndDate =  this.formatDate(actualDate);
+    this.formattedStartDate = this.formatDate(dateFromMonthAgo);
+  }
 
   changeDate(event: any, isEndDate: boolean = false) {
     if(isEndDate) {
@@ -16,4 +30,13 @@ export class TablesComponent {
       this.selectedStartDate = event.value;
     }
   }
+  formatDate = (date: Date): string => {
+    const year = date.getFullYear();
+    const month = (date.getMonth() + 1).toString().padStart(2, '0');
+    // const day = (date.getDate() + 1).toString().padStart(2, '0');  // jeżeli kalendarz wskazuje date o 1 mneijsza
+    const day = (date.getDate()).toString().padStart(2, '0');         // jeżeli kalendarz wskazuje date poprawnie
+    return `${year}-${month}-${day}`;
+  };
 }
+
+
