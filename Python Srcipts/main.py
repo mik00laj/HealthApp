3be87b9c1a8d30c29a1268b31e6641d3e38d387e
mik00laj <mikolaj.bania@gmail.com>
2024-03-27 15:23:37 +0100

    ppython scripts

--- Python Srcipts/main.py
+++ Python Srcipts/main.py
@@ -8,26 +8,31 @@ try:
     print("Succeeded generateBodyTemperatureData.py")
 except Exception as error:
     print(f"Failed generateBodyTemperatureData.py: {error}")
+
 try:
     exec(open('generateBloodSaturationData.py').read())
     print("Succeeded generateBloodSaturationData.py")
 except Exception as error:
     print(f"Failed generateBloodSaturationData.py: {error}")
+
 try:
     exec(open('generateHearthRateData.py').read())
     print("Succeeded generateHearthRateData.py")
 except Exception as error:
     print(f"Failed generateHearthRateData.py: {error}")
+
 try:
     exec(open('generateBodyWeightData.py').read())
     print("Succeeded generateBodyWeightData.py")
 except Exception as error:
     print(f"Failed generateBodyWeightData.py: {error}")
+
 try:
     exec(open('generateRespirationRateData.py').read())
     print("Succeeded generateRespirationRateData.py")
 except Exception as error:
     print(f"Failed generateRespirationRateData.py: {error}")
+
 try:
     exec(open('generateBloodPressureData.py').read())
     print("Succeeded generateBloodPressureData.py")
